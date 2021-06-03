da8876ad3e23fc0c8e2701ae532a953382f2ae09
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-02 21:27:45 -0300

    Adicionado type entity

---
+++ src/main/java/br/com/marcelomsilva/backendtestjava/entity/Type.java
@@ -0,0 +1,26 @@
+package br.com.marcelomsilva.backendtestjava.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "type")
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
