f0a7f29c7d6e2a6772c4e74ccabacd033468b9df
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-02 21:05:17 -0300

    Adicionado arquivos iniciais

---
+++ src/test/java/br/com/marcelomsilva/backendtestjava/BackendTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelomsilva.backendtestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
