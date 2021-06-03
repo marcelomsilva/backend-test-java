d21b178c56d264442f4c96f778fc5edbff8581b2
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-02 21:51:40 -0300

    Adicionado vacancy entity

---
+++ src/main/java/br/com/marcelomsilva/backendtestjava/entity/Vacancy.java
@@ -0,0 +1,38 @@
+package br.com.marcelomsilva.backendtestjava.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vacancy")
+public class Vacancy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer amount;
+    private Type type;
+    private Parking parking;
+
+    public Vacancy(Integer amount, Type type, Parking parking) {
+        this.amount = amount;
+        this.type = type;
+        this.parking = parking;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Parking getParking() {
+        return parking;
+    }
+}
