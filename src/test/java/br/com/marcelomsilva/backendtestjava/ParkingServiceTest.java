28e444d81baa5845d060a9d6074f349a7d858710
marcelomsilva <marcelo96comercial@gmail.com>
2021-06-05 17:06:07 -0300

    Adicionado metodo createTest no ParkingServiceTest

---
+++ src/test/java/br/com/marcelomsilva/backendtestjava/ParkingServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.marcelomsilva.backendtestjava;
+
+import br.com.marcelomsilva.backendtestjava.dto.form.ParkingForm;
+import br.com.marcelomsilva.backendtestjava.service.ParkingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ParkingServiceTest {
+
+    @Autowired
+    ParkingService service;
+
+    @Test
+    public void createTest() {
+        ParkingForm form = new ParkingForm("Nome Teste", "123456", "123", "Rua Joao da Costa",
+                "321", "Sao Paulo", "SP", "Jardim Cruz", "");
+        assertEquals(200, service.create(form).getStatusCodeValue());
+    }
+}
