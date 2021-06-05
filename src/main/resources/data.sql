6cc8fd1af4699b218849c622b71000910b632992
marcelomsilva <marcelo96comercial@gmail.com>
2021-06-04 21:43:44 -0300

    Adicionado data.sql e ajustado application.properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into brand(name) values('Chevrolet');
+insert into brand(name) values('Volkswagens');
+insert into brand(name) values('Yamaha');
+insert into brand(name) values('Honda');
+
+insert into type(name) values('Carro');
+insert into type(name) values('Moto');
+
+insert into model(name, brand_id, type_id) values('Fazer 150', 3, 2);
+insert into model(name, brand_id, type_id) values('Lander 250', 3, 2);
+insert into model(name, brand_id, type_id) values('Biz 100', 4, 2);
+insert into model(name, brand_id, type_id) values('CB 650f', 4, 2);
+insert into model(name, brand_id, type_id) values('Gol', 2, 1);
+insert into model(name, brand_id, type_id) values('Virtus', 2, 1);
+insert into model(name, brand_id, type_id) values('Onix', 1, 1);
+insert into model(name, brand_id, type_id) values('Camaro', 1, 1);
---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+insert into brand(name) values('Chevrolet');
+insert into brand(name) values('Volkswagens');
+insert into brand(name) values('Yamaha');
+insert into brand(name) values('Honda');
+
+insert into type(name) values('Carro');
+insert into type(name) values('Moto');
+
+insert into model(name, brand_id, type_id) values('Fazer 150', 3, 2);
