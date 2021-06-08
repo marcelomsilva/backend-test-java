bd4097593bd9fe80c781c9e2f549f8a453d94525
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-08 19:45:17 -0300

    Ajustado VacancyService createTest

--- src/test/java/br/com/marcelomsilva/backendtestjava/VacancyServiceTest.java
+++ src/test/java/br/com/marcelomsilva/backendtestjava/VacancyServiceTest.java
@@ -6,15 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class VacancyServiceTest {
 
     @Autowired
-    VacancyService vacancyService;
+    VacancyService service;
 
     @Test
     public void create() {
-        vacancyService.create(new VacancyForm(20, 1L, 1L));
-
+        assertEquals(200, service.create(new VacancyForm(20, 1L, 1L)).getStatusCodeValue());
     }
 }
