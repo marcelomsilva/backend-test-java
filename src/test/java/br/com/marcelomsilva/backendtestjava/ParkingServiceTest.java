257675bbd0042667b459689b24d10b8fc542ae1a
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-12 17:03:43 -0300

    Ajustado ParkingServiceTest

--- src/test/java/br/com/marcelomsilva/backendtestjava/ParkingServiceTest.java
+++ src/test/java/br/com/marcelomsilva/backendtestjava/ParkingServiceTest.java
@@ -1,10 +1,19 @@
 package br.com.marcelomsilva.backendtestjava;
 
+import br.com.marcelomsilva.backendtestjava.dto.ParkingDto;
 import br.com.marcelomsilva.backendtestjava.dto.form.ParkingForm;
+import br.com.marcelomsilva.backendtestjava.entity.Address;
+import br.com.marcelomsilva.backendtestjava.entity.Parking;
+import br.com.marcelomsilva.backendtestjava.entity.Phone;
 import br.com.marcelomsilva.backendtestjava.service.ParkingService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,17 +23,18 @@ public class ParkingServiceTest {
     @Autowired
     ParkingService service;
 
-
-
     @Test
     public void createTest() {
-        ParkingForm form = new ParkingForm("Nome Teste", "123456", "123", "Rua Joao da Costa",
-                "321", "Sao Paulo", "SP", "Jardim Cruz", "", "12", "4234234");
+        ParkingForm form = new ParkingForm("Teste", "123454856", "123488", "Rua Joao da Costa 2",
+                "321", "Sao Paulo", "SP", "Jardim Cruz y", "", "1256", "4234234989");
         assertEquals(200, service.create(form).getStatusCodeValue());
     }
 
     @Test
     public void getById() {
-        service.getById(1L);
+        ParkingForm form = new ParkingForm("Nome Teste", "123456", "123", "Rua Joao da Costa",
+                "321", "Sao Paulo", "SP", "Jardim Cruz", "", "12", "4234234");
+        ResponseEntity<ParkingDto> parkingDtoResponseEntity = service.create(form);
+        service.getById(parkingDtoResponseEntity.getBody().getId());
     }
 }
