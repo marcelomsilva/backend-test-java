8ce131b6e5057c0b09e40b87c9959653bf7b41ad
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-13 13:03:36 -0300

    Adicionado id no VehicleControlDTO

--- src/main/java/br/com/marcelomsilva/backendtestjava/dto/VehicleControlDto.java
+++ src/main/java/br/com/marcelomsilva/backendtestjava/dto/VehicleControlDto.java
@@ -7,6 +7,7 @@ import java.time.Instant;
 
 public class VehicleControlDto {
 
+    private Long id;
     private VehicleDto vehicle;
     private Instant entry;
     private Instant departure;
@@ -14,10 +15,15 @@ public class VehicleControlDto {
 
 
     public VehicleControlDto(VehicleControl vehicleControl) {
+        this.id = vehicleControl.getId();
         this.vehicle = new VehicleDto(vehicleControl.getVehicle());
         this.entry = vehicleControl.getEntry();
         this.departure = vehicleControl.getDeparture();
         this.duration = vehicleControl.getDuration();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public VehicleDto getVehicle() {
