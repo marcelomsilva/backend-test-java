39d97ae285364835ed2fb3bb47850020e547279a
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-16 18:07:28 -0300

    Adicionado teste para o update da entidade Parking

--- src/test/java/br/com/marcelomsilva/backendtestjava/ParkingServiceTest.java
+++ src/test/java/br/com/marcelomsilva/backendtestjava/ParkingServiceTest.java
@@ -2,6 +2,7 @@ package br.com.marcelomsilva.backendtestjava;
 
 import br.com.marcelomsilva.backendtestjava.dto.ParkingDto;
 import br.com.marcelomsilva.backendtestjava.dto.form.ParkingForm;
+import br.com.marcelomsilva.backendtestjava.dto.form.ParkingUpdateForm;
 import br.com.marcelomsilva.backendtestjava.entity.Address;
 import br.com.marcelomsilva.backendtestjava.entity.Parking;
 import br.com.marcelomsilva.backendtestjava.entity.Phone;
@@ -25,23 +26,36 @@ public class ParkingServiceTest {
 
     @Test
     public void createTest() {
-        ParkingForm form = new ParkingForm("Teste", "123454856", "123488", "Rua Joao da Costa 2",
+        ParkingForm form = new ParkingForm("Teste", "123454856", "12348899", "Rua Joao da Costa 2",
                 "321", "Sao Paulo", "SP", "Jardim Cruz y", "", "1256", "4234234989");
         assertEquals(200, service.create(form).getStatusCodeValue());
     }
 
     @Test
     public void getById() {
-        ParkingForm form = new ParkingForm("Nome Teste", "123456", "123", "Rua Joao da Costa",
+        ParkingForm form = new ParkingForm("Nome Teste", "123456", "12345879", "Rua Joao da Costa",
                 "321", "Sao Paulo", "SP", "Jardim Cruz", "", "12", "4234234");
         ResponseEntity<ParkingDto> parkingDtoResponseEntity = service.create(form);
         service.getById(parkingDtoResponseEntity.getBody().getId());
     }
 
-    @Test void disableById() {
-        ParkingForm form = new ParkingForm("Nome Teste", "123456", "123", "Rua Joao da Costa",
+    @Test
+    public void disableById() {
+        ParkingForm form = new ParkingForm("Nome Teste Novamente", "1234569888", "12345678", "Rua Joao da Costa",
                 "321", "Sao Paulo", "SP", "Jardim Cruz", "", "12", "4234234");
         Long parkingId = service.create(form).getBody().getId();
         service.disableById(parkingId);
+    }
+
+    @Test
+    public void update() {
+        ParkingForm form = new ParkingForm("Nome htrhrt", "12345665464", "12345678", "Rua Joao da Costa",
+                "321", "Sao Paulo Alto", "SP", "Jardim Souza lima Cruz", "", "125435", "4235534234");
+        ParkingDto parking = service.create(form).getBody();
+        ParkingUpdateForm formUpdate = new ParkingUpdateForm("Nome teste", "12345678", "12345678", "Rua Joao",
+                "321", "Sao Paulo Baixo", "RJ", "448", "teste");
+        ParkingDto parkingUpdate = service.update(parking.getId(), formUpdate).getBody();
+        assertEquals(false , parking.equals(parkingUpdate));
+
     }
 }
