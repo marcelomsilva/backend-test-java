ece31d96814d48ccaeb8fe62a5406da8c2a90d68
marcelomsilva <marcelo.silva@fcamara.com.br>
2021-06-22 17:46:25 -0300

    Ajustado PhoneDto

--- src/main/java/br/com/marcelomsilva/backendtestjava/dto/PhoneDto.java
+++ src/main/java/br/com/marcelomsilva/backendtestjava/dto/PhoneDto.java
@@ -8,12 +8,14 @@ public class PhoneDto {
     private String code;
     private String number;
     private Long parkingId;
+    private String parkingName;
 
     public PhoneDto(Phone phone) {
         this.id = phone.getId();
         this.code = phone.getCode();
         this.number = phone.getNumber();
         this.parkingId = phone.getParking().getId();
+        this.parkingName = phone.getParking().getName();
     }
 
     public Long getId() {
@@ -30,5 +32,9 @@ public class PhoneDto {
 
     public Long getParkingId() {
         return parkingId;
+    }
+
+    public String getParkingName() {
+        return parkingName;
     }
 }
